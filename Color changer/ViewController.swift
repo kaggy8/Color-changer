941f7d75a4ea1bceffbb820d724746ad148f32b8
Stanislav <kaggy8@ya.ru>
2022-04-04 22:25:03 +0300

    Fix bugs =)

--- Color changer/ViewController.swift
+++ Color changer/ViewController.swift
@@ -19,12 +19,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var greenValueLabel: UILabel!
     @IBOutlet weak var blueValueLabel: UILabel!
     
-    // MARK: - Private properties
-    
-    private var redNumber: CGFloat = 0
-    private var greenNumber: CGFloat = 0
-    private var blueNumber: CGFloat = 0
-    
     // MARK: - Life cycle
     
     override func viewDidLoad() {
@@ -35,25 +29,23 @@ class ViewController: UIViewController {
         redSliderSet()
         greenSliderSet()
         blueSliderSet()
+        setValueLabel()
+        changeColor()
     }
     
     // MARK: - IBActions
     
     @IBAction func changeOfRed(_ sender: UISlider) {
-        redNumber = CGFloat(sender.value)
         redValueLabel.text = String(format: "%.0f", sender.value)
-        
         changeColor()
     }
     
     @IBAction func changeOfGreen(_ sender: UISlider) {
-        greenNumber = CGFloat(sender.value)
         greenValueLabel.text = String(format: "%.0f", sender.value)
         changeColor()
     }
     
     @IBAction func changeOfBlue(_ sender: UISlider) {
-        blueNumber = CGFloat(sender.value)
         blueValueLabel.text = String(format: "%.0f", sender.value)
         changeColor()
     }
@@ -61,26 +53,32 @@ class ViewController: UIViewController {
     // MARK: - Private Methods
     
     private func redSliderSet() {
-        redSlider.value = 255
+        redSlider.setValue(100, animated: true)
         redSlider.maximumValue = 255
         redSlider.minimumTrackTintColor = .red
     }
     
     private func greenSliderSet() {
-        greenSlider.value = 255
+        greenSlider.setValue(144, animated: true)
         greenSlider.maximumValue = 255
         greenSlider.minimumTrackTintColor = .green
     }
     
     private func blueSliderSet() {
-        blueSlider.value =  255
+        blueSlider.setValue(235, animated: true)
         blueSlider.maximumValue = 255
         blueSlider.minimumTrackTintColor = .blue
     }
     
-    private func changeColor() {
-        colorMonitor.backgroundColor = UIColor(red: redNumber/255, green: greenNumber/255, blue: blueNumber/255, alpha: 1)
+    private func setValueLabel() {
+        redValueLabel.text = String(format: "%.0f", redSlider.value)
+        greenValueLabel.text = String(format: "%.0f", greenSlider.value)
+        blueValueLabel.text = String(format: "%.0f", blueSlider.value)
     }
     
+    private func changeColor() {
+        colorMonitor.backgroundColor = UIColor(red: CGFloat(redSlider.value)/255, green: CGFloat(greenSlider.value)/255, blue: CGFloat(blueSlider.value)/255, alpha: 1)
+    }
+
 }
 
